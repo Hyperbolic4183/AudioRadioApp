6320d0ff64cdca351c6f0ec662ad856eec9bbf26
otsuka.amane <otsuka.amane@gmail.com>
2021-01-04 13:52:31 +0900

    RecordingModelにaudioSessionのカテゴリを設定し、アクティブ化する関数を作成した。

--- AudioRecordingApp/Model/RecordingModel.swift
+++ AudioRecordingApp/Model/RecordingModel.swift
@@ -9,11 +9,36 @@ import UIKit
 import AVFoundation
 
 struct RecordingModel {
+    var audioRecoder: AVAudioRecorder!
+    var audioPlayer: AVAudioPlayer!
     
     func record() {
         print("record")
+        let audioSession = AVAudioSession.sharedInstance()
+        //許可を求めるアラートを表示する
+        audioSession.requestRecordPermission { granted in
+            if granted {
+                print("許可した")
+                do {
+                    //audioSessionのカテゴリを設定
+                    try audioSession.setCategory(.record, mode: .default, options: [])
+                } catch {
+                    print("audioSessionがカテゴリのセットに失敗した")
+                }
+            } else {
+                print("許可しなかった")
+                return
+            }
+        }
+        do {
+            try audioSession.setActive(true)
+        } catch {
+            print("audioSessionのアクティブ可に失敗した")
+            return
+        }
     }
-    func stop() {
-        print("stop")
-    }
+
 }
+
+
+
